cb7a134d4b8f2ccf78d28e3acae64d6401dac6f5
oliver <oliver.burkhard@gmail.com>
2021-03-28 17:00:04 +0200

    Finish calculations (first time)

--- src/all_exposure.py
+++ src/all_exposure.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from src.accounts import get_account_balances
@@ -5,15 +6,26 @@ from src.isin_positions import get_isin_pos
 from src.all_values import get_all_historic_values
 
 
-def get_all_exposure(values: pd.DataFrame = get_all_historic_values()):
+def get_all_exposure(values: pd.DataFrame = get_all_historic_values(), normalise: bool = False):
     """
     Gets the exposure of every date where the valuations are available and of every observed instrument in nummeraire
     currency
 
     :param values: (date)[|<cur1>]+[|<isin1>] valuations, must contain all instruments in possession
+    :param normalise: if True, values are normalised to sum to 1
     :return: (date)[|<cur1>]+[|<isin1>]
     """
     cash = get_account_balances()
     investments = get_isin_pos()
     portfolio = cash.merge(investments, left_index=True, right_index=True, how="outer")
     assert all(c in values.columns for c in portfolio.columns)
+    relevant_values = values[portfolio.columns].merge(
+        portfolio, left_index=True, right_index=True, how="left", suffixes=["_val", "_pos"])
+    for c in [c for c in relevant_values.columns if c.endswith("_pos")]:
+        relevant_values[c].fillna(method="ffill", inplace=True)
+    for c in portfolio.columns:
+        relevant_values[c + "_tot"] = np.where(relevant_values[c + "_pos"] == 0, 0,
+                                               relevant_values[c + "_val"] * relevant_values[c + "_pos"])
+    exposure = relevant_values[[c for c in relevant_values if c.endswith("_tot")]]
+    exposure.columns = [c.replace("_tot", "") for c in exposure.columns]
+    return exposure if not normalise else exposure.div(exposure.sum(axis=1, min_count=1), axis=0)
---
+++ src/performance.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+from src.all_values import get_all_historic_values
+from src.all_exposure import get_all_exposure
+
+
+def get_portfolio_performance(
+        values: pd.DataFrame = get_all_historic_values("chf"),
+        exposure: pd.DataFrame = get_all_exposure(normalise=False),
+        market: str = "ie00bym11h29",  # default: acwi
+) -> pd.DataFrame:
+    """
+    Gives CAPM performance measures for the constituents as well as the portfolio overall
+    :param values: valuations of the individual investments: (date)|<name>...
+    :param exposure: total value by investment (date)|<name>... with the same name and same index as values
+    :param market: name of the market instrument
+    :return: (instrument)|return|sd|beta
+    """
+    assert values.shape == exposure.shape
+    assert values.index.equals(exposure.index)
+    values["portfolio"] = exposure.sum(axis=1, min_count=exposure.shape[1])
+    days = pd.bdate_range(min(values.index), max(values.index), freq="C", weekmask="Fri")
+    weekly_values = values.loc[days]
+    returns = np.log10(weekly_values).diff()
+    covariances = returns.cov()
+    betas = covariances[market] / covariances[market][market]
+    avg_returns = returns.mean()
+    sd = np.diagonal(covariances)
+    return pd.DataFrame({"return": avg_returns, "sd": sd, "betas": betas})
